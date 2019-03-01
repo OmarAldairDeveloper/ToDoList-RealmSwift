1dd9848ad075db81c7f45116909a3950e061ca16
Omar Aldair <omaraldair.developer@gmail.com>
2019-02-28 22:59:57 -0600

    Primer commit

---
+++ NotesRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesRealm
+//
+//  Created by Omar Aldair Romero Pérez on 2/28/19.
+//  Copyright © 2019 Omar Aldair Romero Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
