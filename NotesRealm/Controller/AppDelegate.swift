f7134484db3283cea3f87f8622302898f3189f35
Omar Aldair <omaraldair.developer@gmail.com>
2019-02-28 23:24:08 -0600

    Hemos creado los controladores base de la aplicacion

--- NotesRealm/Controller/AppDelegate.swift
+++ NotesRealm/Controller/AppDelegate.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
+
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        
-        
         return true
     }
 
---
+++ NotesRealm/View/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  NotesRealm
+//
+//  Created by Omar Aldair Romero Pérez on 2/28/19.
+//  Copyright © 2019 Omar Aldair Romero Pérez. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    
+}
